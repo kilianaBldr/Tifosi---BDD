fac94d8d45840762a7f9337e4ed3fa8876cd020b
kiliana Bldr <167337564+kilianaBldr@users.noreply.github.com>
2024-10-04 16:08:25 +0200

    Add files via upload

---
+++ tifosi_structure.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 04 oct. 2024 à 14:06
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tifosi`
+--
+
+--
+-- Déchargement des données de la table `boisson`
+--
+
+INSERT INTO `boisson` (`id_boisson`, `nom_boisson`, `id_marque`) VALUES
+(1, 'Coca-Cola zéro', 1),
+(2, 'Coca-Cola original', 1),
+(3, 'Fanta citron', 1),
+(4, 'Fanta orange', 1),
+(5, 'Capri-sun', 1),
+(5, 'Pepsi', 4),
+(6, 'Pepsi Max Zéro', 4),
+(7, 'Lipton zéro citron', 4),
+(8, 'Lipton Peach', 4),
+(9, 'Monster energy utra gold', 3),
+(10, 'Monster energy utra blue', 3),
+(11, 'Eau de source', 2);
+
+--
+-- Déchargement des données de la table `comprend`
+--
+
+INSERT INTO `comprend` (`id_focaccia`, `id_ingredient`) VALUES
+(1, 1),
+(1, 3),
+(1, 5),
+(1, 7),
+(1, 9),
+(1, 13),
+(1, 16),
+(1, 18),
+(1, 20),
+(1, 25),
+(2, 1),
+(2, 5),
+(2, 7),
+(2, 9),
+(2, 11),
+(2, 16),
+(2, 20),
+(2, 25),
+(3, 1),
+(3, 5),
+(3, 7),
+(3, 9),
+(3, 20),
+(3, 22),
+(3, 25),
+(4, 6),
+(4, 7),
+(4, 9),
+(4, 10),
+(4, 15),
+(4, 18),
+(4, 20),
+(5, 5),
+(5, 7),
+(5, 9),
+(5, 12),
+(5, 16),
+(5, 17),
+(5, 18),
+(5, 20),
+(5, 25),
+(6, 2),
+(6, 4),
+(6, 5),
+(6, 9),
+(6, 16),
+(6, 18),
+(6, 19),
+(6, 20),
+(6, 25),
+(7, 4),
+(7, 5),
+(7, 9),
+(7, 16),
+(7, 18),
+(7, 20),
+(7, 21),
+(7, 25),
+(8, 1),
+(8, 3),
+(8, 6),
+(8, 7),
+(8, 8),
+(8, 9),
+(8, 13),
+(8, 14),
+(8, 16),
+(8, 18),
+(8, 20),
+(8, 21);
+
+--
+-- Déchargement des données de la table `focaccia`
+--
+
+INSERT INTO `focaccia` (`id_focaccia`, `nom_focaccia`, `prix_focaccia`) VALUES
+(1, 'Mozaccia', 9.8),
+(2, 'Gorgonzollaccia', 10.8),
+(3, 'Raclaccia', 8.9),
+(4, 'Emmentalaccia', 9.8),
+(5, 'Tradizione', 8.9),
+(6, 'Hawaienne', 11.2),
+(7, 'Américaine', 10.8),
+(8, 'Paysanne', 12.8);
+
+--
+-- Déchargement des données de la table `ingredient`
+--
+
+INSERT INTO `ingredient` (`id_ingredient`, `nom_ingredient`) VALUES
+(1, 'Ail'),
+(2, 'Ananas'),
+(3, 'Artichaut'),
+(4, 'Bacon'),
+(5, 'Base tomate'),
+(6, 'Base crème'),
+(7, 'Champignon'),
+(8, 'Chevre'),
+(9, 'Cresson'),
+(10, 'Emmentale'),
+(11, 'Gorgonzola'),
+(12, 'Jambon cuit'),
+(13, 'Jambon fumé'),
+(14, 'Oeuf'),
+(15, 'Oignon'),
+(16, 'Olive noire'),
+(17, 'Olive verte'),
+(18, 'Parmesan'),
+(19, 'Piment'),
+(20, 'Poivre'),
+(21, 'Pomme de terre'),
+(22, 'Raclette'),
+(23, 'Salami'),
+(24, 'Tomate cerise'),
+(25, 'Mozarella');
+
+--
+-- Déchargement des données de la table `marque`
+--
+
+INSERT INTO `marque` (`id_marque`, `nom_marque`) VALUES
+(1, 'Coca-Cola'),
+(2, 'Cristalline'),
+(3, 'Monster'),
+(4, '[Pepsico');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
