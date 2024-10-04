06259f81e4417fb343b6f10d83a939832ed06315
kiliana Bldr <167337564+kilianaBldr@users.noreply.github.com>
2024-10-04 15:05:43 +0200

    Backup_tifosi

---
+++ backup_tifosi.sql
@@ -0,0 +1,338 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: tifosi
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `achete`
+--
+
+DROP TABLE IF EXISTS `achete`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `achete` (
+  `id_client` int(11) DEFAULT NULL,
+  `id_focaccia` int(11) DEFAULT NULL,
+  `jour` date NOT NULL,
+  KEY `id_client` (`id_client`),
+  KEY `id_focaccia` (`id_focaccia`),
+  CONSTRAINT `achete_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`),
+  CONSTRAINT `achete_ibfk_2` FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia` (`id_focaccia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `achete`
+--
+
+LOCK TABLES `achete` WRITE;
+/*!40000 ALTER TABLE `achete` DISABLE KEYS */;
+/*!40000 ALTER TABLE `achete` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `appartient`
+--
+
+DROP TABLE IF EXISTS `appartient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `appartient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_boisson` int(11) DEFAULT NULL,
+  `id_marque` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_boisson` (`id_boisson`),
+  KEY `id_marque` (`id_marque`),
+  CONSTRAINT `appartient_ibfk_1` FOREIGN KEY (`id_boisson`) REFERENCES `boisson` (`id_boisson`),
+  CONSTRAINT `appartient_ibfk_2` FOREIGN KEY (`id_marque`) REFERENCES `marque` (`id_marque`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appartient`
+--
+
+LOCK TABLES `appartient` WRITE;
+/*!40000 ALTER TABLE `appartient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `appartient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `boisson`
+--
+
+DROP TABLE IF EXISTS `boisson`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `boisson` (
+  `id_boisson` int(11) NOT NULL,
+  `nom_boisson` varchar(45) DEFAULT NULL,
+  `id_marque` int(11) NOT NULL,
+  PRIMARY KEY (`id_boisson`,`id_marque`),
+  KEY `id_marque` (`id_marque`),
+  CONSTRAINT `boisson_ibfk_1` FOREIGN KEY (`id_marque`) REFERENCES `marque` (`id_marque`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `boisson`
+--
+
+LOCK TABLES `boisson` WRITE;
+/*!40000 ALTER TABLE `boisson` DISABLE KEYS */;
+INSERT INTO `boisson` VALUES (1,'Coca-Cola zéro',1),(2,'Coca-Cola original',1),(3,'Fanta citron',1),(4,'Fanta orange',1),(5,'Capri-sun',1),(5,'Pepsi',4),(6,'Pepsi Max Zéro',4),(7,'Lipton zéro citron',4),(8,'Lipton Peach',4),(9,'Monster energy utra gold',3),(10,'Monster energy utra blue',3),(11,'Eau de source',2);
+/*!40000 ALTER TABLE `boisson` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `id_client` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_client` varchar(45) NOT NULL,
+  `age` int(11) NOT NULL,
+  `cp_client` int(11) NOT NULL,
+  PRIMARY KEY (`id_client`),
+  CONSTRAINT `client_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `paye` (`id_client`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comprend`
+--
+
+DROP TABLE IF EXISTS `comprend`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comprend` (
+  `id_focaccia` int(11) NOT NULL,
+  `id_ingredient` int(11) NOT NULL,
+  PRIMARY KEY (`id_focaccia`,`id_ingredient`),
+  KEY `id_ingredient` (`id_ingredient`),
+  CONSTRAINT `comprend_ibfk_1` FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia` (`id_focaccia`),
+  CONSTRAINT `comprend_ibfk_2` FOREIGN KEY (`id_ingredient`) REFERENCES `ingredient` (`id_ingredient`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comprend`
+--
+
+LOCK TABLES `comprend` WRITE;
+/*!40000 ALTER TABLE `comprend` DISABLE KEYS */;
+INSERT INTO `comprend` VALUES (1,1),(1,3),(1,5),(1,7),(1,9),(1,13),(1,16),(1,18),(1,20),(1,25),(2,1),(2,5),(2,7),(2,9),(2,11),(2,16),(2,20),(2,25),(3,1),(3,5),(3,7),(3,9),(3,20),(3,22),(3,25),(4,6),(4,7),(4,9),(4,10),(4,15),(4,18),(4,20),(5,5),(5,7),(5,9),(5,12),(5,16),(5,17),(5,18),(5,20),(5,25),(6,2),(6,4),(6,5),(6,9),(6,16),(6,18),(6,19),(6,20),(6,25),(7,4),(7,5),(7,9),(7,16),(7,18),(7,20),(7,21),(7,25),(8,1),(8,3),(8,6),(8,7),(8,8),(8,9),(8,13),(8,14),(8,16),(8,18),(8,20),(8,21);
+/*!40000 ALTER TABLE `comprend` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contient`
+--
+
+DROP TABLE IF EXISTS `contient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contient` (
+  `id_menu` int(11) NOT NULL,
+  `id_boisson` int(11) NOT NULL,
+  PRIMARY KEY (`id_menu`,`id_boisson`),
+  KEY `id_boisson` (`id_boisson`),
+  CONSTRAINT `contient_ibfk_1` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`),
+  CONSTRAINT `contient_ibfk_2` FOREIGN KEY (`id_boisson`) REFERENCES `boisson` (`id_boisson`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contient`
+--
+
+LOCK TABLES `contient` WRITE;
+/*!40000 ALTER TABLE `contient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `est_constitue`
+--
+
+DROP TABLE IF EXISTS `est_constitue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `est_constitue` (
+  `id_focaccia` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  PRIMARY KEY (`id_focaccia`,`id_menu`),
+  KEY `id_menu` (`id_menu`),
+  CONSTRAINT `est_constitue_ibfk_1` FOREIGN KEY (`id_focaccia`) REFERENCES `focaccia` (`id_focaccia`),
+  CONSTRAINT `est_constitue_ibfk_2` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `est_constitue`
+--
+
+LOCK TABLES `est_constitue` WRITE;
+/*!40000 ALTER TABLE `est_constitue` DISABLE KEYS */;
+/*!40000 ALTER TABLE `est_constitue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `focaccia`
+--
+
+DROP TABLE IF EXISTS `focaccia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `focaccia` (
+  `id_focaccia` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_focaccia` varchar(45) NOT NULL,
+  `prix_focaccia` float NOT NULL,
+  PRIMARY KEY (`id_focaccia`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `focaccia`
+--
+
+LOCK TABLES `focaccia` WRITE;
+/*!40000 ALTER TABLE `focaccia` DISABLE KEYS */;
+INSERT INTO `focaccia` VALUES (1,'Mozaccia',9.8),(2,'Gorgonzollaccia',10.8),(3,'Raclaccia',8.9),(4,'Emmentalaccia',9.8),(5,'Tradizione',8.9),(6,'Hawaienne',11.2),(7,'Américaine',10.8),(8,'Paysanne',12.8);
+/*!40000 ALTER TABLE `focaccia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredient`
+--
+
+DROP TABLE IF EXISTS `ingredient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ingredient` (
+  `id_ingredient` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_ingredient` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_ingredient`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredient`
+--
+
+LOCK TABLES `ingredient` WRITE;
+/*!40000 ALTER TABLE `ingredient` DISABLE KEYS */;
+INSERT INTO `ingredient` VALUES (1,'Ail'),(2,'Ananas'),(3,'Artichaut'),(4,'Bacon'),(5,'Base tomate'),(6,'Base crème'),(7,'Champignon'),(8,'Chevre'),(9,'Cresson'),(10,'Emmentale'),(11,'Gorgonzola'),(12,'Jambon cuit'),(13,'Jambon fumé'),(14,'Oeuf'),(15,'Oignon'),(16,'Olive noire'),(17,'Olive verte'),(18,'Parmesan'),(19,'Piment'),(20,'Poivre'),(21,'Pomme de terre'),(22,'Raclette'),(23,'Salami'),(24,'Tomate cerise'),(25,'Mozarella');
+/*!40000 ALTER TABLE `ingredient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marque`
+--
+
+DROP TABLE IF EXISTS `marque`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marque` (
+  `id_marque` int(11) NOT NULL,
+  `nom_marque` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_marque`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marque`
+--
+
+LOCK TABLES `marque` WRITE;
+/*!40000 ALTER TABLE `marque` DISABLE KEYS */;
+INSERT INTO `marque` VALUES (1,'Coca-Cola'),(2,'Cristalline'),(3,'Monster'),(4,'[Pepsico');
+/*!40000 ALTER TABLE `marque` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu` (
+  `id_menu` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_menu` varchar(45) NOT NULL,
+  `prix_menu` float NOT NULL,
+  PRIMARY KEY (`id_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paye`
+--
+
+DROP TABLE IF EXISTS `paye`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paye` (
+  `id_client` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `jour` date NOT NULL DEFAULT curdate(),
+  PRIMARY KEY (`id_client`,`id_menu`),
+  KEY `id_menu` (`id_menu`),
+  CONSTRAINT `paye_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`),
+  CONSTRAINT `paye_ibfk_2` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paye`
+--
+
+LOCK TABLES `paye` WRITE;
+/*!40000 ALTER TABLE `paye` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paye` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-04 15:02:17
